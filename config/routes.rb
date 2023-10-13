4170cc61e392fa8c2949183212ce10dee2ee13ca
Sumit Bansal <sbansal.mait@gmail.com>
2023-10-13 23:41:44 +0530

    Scheduled jobs for air quality metric data update.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
   # root "articles#index"
 end
